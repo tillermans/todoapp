2e9f1319376f41d9b2f969ded9d31c555488790a
tillermans <166988089+tillermans@users.noreply.github.com>
2024-04-20 01:06:26 +0100

    Update urls.py
    Adding user signups and login to the app (todoapp)

--- todolist/urls.py
+++ todolist/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('add', views.addTodoItem, name='add'),
     path('completed/<todo_id>', views.completeTodoItem, name='completed'),
     path('delected_completed', views.deleteCompleted, name='deleteCompleted'),
-    path('deleteall', views.deleteAll, name='deleteAll')
+    path('deleteall', views.deleteAll, name='deleteAll'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('profile/', views.profile, name='profile'),
 ]
